f94052e61bf696178488352406afe0205fb8859f
Cinkowyy <90154960+Cinkowyy@users.noreply.github.com>
2022-06-29 13:59:26 +0200

    refactor: change home scrren and songs container styles

--- src/components/SongCard/SongCard.tsx
+++ src/components/SongCard/SongCard.tsx
@@ -8,13 +8,13 @@ const SongCard: React.FC<Song> = (props) => {
         src={props.artworkUrl100}
         alt="artwork"
       />
-      <div className="cardContent">
+      <div className="songCard__content">
         <div className="songDetails">
           <p className="songDetails__track">{props.trackName}</p>
           <p className="songDetails__artist">{props.artistName}</p>
         </div>
 
-        <div className="previewPlayer">Player here</div>
+        <div className="songCard__player">Player here</div>
       </div>
     </div>
   );
